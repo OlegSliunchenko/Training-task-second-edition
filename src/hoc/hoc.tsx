d22dc7ed4feecbde322a5158384728c21f0eb1f5
Oleg Sliunchenko <oleg.sliuncheko@gmail.com>
2022-07-10 20:09:25 +0300

    HOC Component Added

--- src/hoc/hoc.tsx
+++ src/hoc/hoc.tsx
@@ -1,18 +1,21 @@
-import React, { useEffect } from 'react';
+import React, { ComponentType } from 'react';
 
-// interface newProps {
-//   param1: string;
-//   param2: string;
-// }
+const hocWrapper = (Component: ComponentType<any>): ComponentType => {
+  // eslint-disable-next-line react-prefer-function-component/react-prefer-function-component
+  return class extends React.Component<any, any> {
+    componentDidUpdate(prevProps: Readonly<any>): void {
+      console.log(prevProps);
+      console.log(`${Component.displayName} was updated`);
+      // eslint-disable-next-line no-restricted-syntax
+      for (const [key, value] of Object.entries(this.props)) {
+        console.log(`${key}: ${value}`);
+      }
+    }
 
-const hocWrapper = (Component: any): any => {
-  return (...props: any) => {
-    useEffect(() => {
-      console.log(`${Component.displayName} was updated with params:`);
-      console.log(``);
-    });
-    // eslint-disable-next-line react/jsx-props-no-spreading
-    return <Component {...props} />;
+    render(): JSX.Element {
+      // eslint-disable-next-line react/jsx-props-no-spreading
+      return <Component {...this.props} />;
+    }
   };
 };
 
