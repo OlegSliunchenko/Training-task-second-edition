ccc94be1898b74fc5dd2463853ed7ed457167172
Oleg Sliunchenko <oleg.sliuncheko@gmail.com>
2022-07-12 11:34:19 +0300

    Transforming objects into Array

--- src/utils/SortingFunction.ts
+++ src/utils/SortingFunction.ts
@@ -3,10 +3,8 @@ import { IActionType, IDataType } from '@utils/type';
 const SortingFunction = (data: IDataType[], action: IActionType): void => {
   switch (action.type) {
     case 'user':
-      // eslint-disable-next-line no-plusplus
-      for (let i = 0; i < data.length; i++) {
-        // eslint-disable-next-line no-plusplus
-        for (let j = 0; j < data.length - i - 1; j++) {
+      for (let i = 0; i < data.length; i += 1) {
+        for (let j = 0; j < data.length - i - 1; j += 1) {
           if (data[j].user > data[j + 1].user) {
             const temp = data[j];
             // eslint-disable-next-line no-param-reassign
@@ -16,13 +14,10 @@ const SortingFunction = (data: IDataType[], action: IActionType): void => {
           }
         }
       }
-      console.log('Sort by User', data);
       break;
     case 'user age':
-      // eslint-disable-next-line no-plusplus
-      for (let i = 0; i < data.length; i++) {
-        // eslint-disable-next-line no-plusplus
-        for (let j = 0; j < data.length - i - 1; j++) {
+      for (let i = 0; i < data.length; i += 1) {
+        for (let j = 0; j < data.length - i - 1; j += 1) {
           if (
             data[j].user > data[j + 1].user
             || data[j].age > data[j + 1].age
@@ -35,10 +30,15 @@ const SortingFunction = (data: IDataType[], action: IActionType): void => {
           }
         }
       }
-      console.log('Sort by User & age', data);
       break;
     default:
       console.log(data);
   }
+  const list = [];
+  for (let i = 0; i < data.length; i += 1) {
+    const NewData = Object.values(data[i]);
+    list.push(NewData);
+  }
+  console.log(list);
 };
 export default SortingFunction;
